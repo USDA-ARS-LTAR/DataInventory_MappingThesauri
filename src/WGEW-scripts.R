58153029be3fb6551ec1361bcca7f6f9b73dd615
Gerardo <garmendariz@gmail.com>
2019-04-09 12:29:32 -0700

    Added workflow to get and push updates from and to the inventory Google Sheet.

--- src/WGEW-scripts.R
+++ src/WGEW-scripts.R
@@ -1,14 +1,79 @@
+#############
 # This script will be used to subset the LTAR inventory data for WGEW
 source("src/inventory-functions.R")
 
+#############
+# Get original data inventory
 df <- get.inventory()
+# add an ID column in order to match with Google Sheet
+df <- cbind(ID = "", df)
 
-df.GS <- get.inventory.from.GoolgeSheets()
+#############
+# Get invenotry from Google Sheets
+df.GS <- get.inventory.from.GoolgeSheets("alldata_2019_(4/9/2019)")
 
+#############
+# subset the GB inventory from the original inventory
+df.site.GB <- subset(df,LTARSite.Code == "GB")
+
+# merge rows from Grate Basin
+df.GS.with.site.GB <- rbind(df.GS, df.site.GB)
+
+#  dataframe with the NAL Subjects and ISO Topics as new columns
+df.GS.updated <- update.inventory.with.NALSubject.and.ISOTopic(df.GS.with.site.GB)
+
+# reset the ID column
+df.GS.updated$ID <- seq.int(nrow(df.GS.updated))
+
+# write new CSV file.  This will be uploaded to the Google Sheet
+write.csv.inventory(df.GS.updated)
+
+
+
+
+
+
+#############
+# Push inventory to new Google Sheet tab. 
+# TODO: This is not currently working. Possibly will be solved in 
+#       googlesheets 0.4.0
+upload.inventory.to.googlesheets(df.GS.updated)
+
+
+
+
+
+
+
+#############
+# Writing CSVs
 df.agcros <- get.AgCROS.domains()
 
 write.csv.agcross(df.agcros) 
-
 write.csv.inventory(df)
-
 write.csv.inventory(df, "WGEW")
+
+
+#############
+# Compare Google Sheets vs R data frame
+
+# local dataframe
+df %>%
+  group_by(LTARSite.Code) %>%
+  summarise(n=n())
+
+# remote (Google Sheets) data frame
+GSdf %>%
+  group_by(LTARSite.Code) %>%
+  summarise(n=n())
+
+
+##########
+# Filter for raw datasets
+df$LTARSite.Code <- rownames(df)
+dplyr::filter(df, grepl('raw', DataSetVariableName))
+# 421 with raw work in DataSetVariableName
+dplyr::filter(df, grepl('raw', AttributeDescription))
+# 39 with raw in AttributeDescription
+
+dplyr::filter(df, grepl('Raw', DataSetVariableName))
