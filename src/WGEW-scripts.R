c700f3136d278d8ad4cfe970793c5373743d3e0f
Gerardo <garmendariz@gmail.com>
2019-05-03 18:01:52 -0700

    Updated the NALT Subject and ISO Topics columns for CPER and KBS.

--- src/WGEW-scripts.R
+++ src/WGEW-scripts.R
@@ -8,8 +8,46 @@ df <- get.inventory()
 # add an ID column in order to match with Google Sheet
 df <- cbind(ID = "", df)
 
+
+
+
+
+
+###############
+#
+# Updating Subject and ISO Topics to KBS and CPER (5/3/2019)
+# 
+##############
+
 #############
 # Get invenotry from Google Sheets
+df.GS <- get.inventory.from.GoolgeSheets("alldata_2019 (before Topics/Subjects to KBS/CPER)")
+
+#############
+# subset the KBS, and CPER inventory from the original inventory to apply subjects and ISO topics
+df.sites <- subset(df.GS,LTARSite.Code == "KBS" | LTARSite.Code == "CPER")
+
+#  dataframe with the NAL Subjects and ISO Topics as new columns
+df.GS.updated <- update.inventory.with.NALSubject.and.ISOTopic(df.sites)
+
+# write new CSV file.  This will be uploaded to the Google Sheet
+write.csv.inventory(df.GS.updated)
+# after creating the CSV for KBS and CPER, I copied and pasted these records to the alldata_2019 sheet
+
+
+
+
+
+
+
+###############
+#
+# Updating Subject and ISO Topics to all datasets (4/9/2019)
+# 
+##############
+
+#############
+# Get invenotry from Google Sheets  
 df.GS <- get.inventory.from.GoolgeSheets("alldata_2019_(4/9/2019)")
 
 #############
@@ -27,6 +65,20 @@ df.GS.updated$ID <- seq.int(nrow(df.GS.updated))
 
 # write new CSV file.  This will be uploaded to the Google Sheet
 write.csv.inventory(df.GS.updated)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
